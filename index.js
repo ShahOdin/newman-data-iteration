94eca9d763b30ed657ba1b71333b7614c82590e5
shahsara <shah.saraei@itv.com>
2021-07-23 11:55:28 +0100

    now can pass env-vars to newman run

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const lineReader = require('line-reader');
 
 require('dotenv').config();
 
+const overrideEnvs = () => {
+    const overrideKeys = process.env.NEWMAN_OVERRIDE_ENVS.split(",");
+    Object.fromEntries(
+        Object
+            .entries(process.env)
+            .filter(([key, value]) => overrideKeys.includes(key))
+    );
+}    
+
 const headers = new Promise(function(resolve, reject) {
     setTimeout(function() {
         lineReader.eachLine(
@@ -40,7 +49,8 @@ const runCollection = csvWriter => {
         reporters: 'cli',
         iterationData: process.env.DATA_PATH,
         delayRequest: process.env.DELAY_REQUEST,
-        environment: process.env.ENV_PATH
+        environment: process.env.ENV_PATH,
+        envVar: [overrideEnvs]
     }).on('test', (error, args) => {
         const result = args.executions[0].result
         let row = result.data;
